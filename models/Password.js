7e43416d5aab6e5d59d0e68eab7393248b01eda8
R3plug <phillipwd95@gmail.com>
2025-05-01 17:46:59 -0700

    Started password route

--- models/Password.js
+++ models/Password.js
@@ -1,5 +1,5 @@
 const mongoose =  require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('crypto-js')
 
 const PasswordSchema = new mongoose.Schema({
     user:String,
@@ -10,21 +10,27 @@ const PasswordSchema = new mongoose.Schema({
 });
 
 
-
+/*
 PasswordSchema.pre('save', async function(next) {
   // Only hash the password if it's modified (or new)
   if (!this.isModified('password')) return next();
   
   try {
-    // Generate a salt
-    const salt = await bcrypt.genSalt(10);
+    const randomBytes = crypto.randomBytes(16);
+
+    let hash = crypto
+        .createHash('sha512')
+        .update(this.password)
+        .update(this.usernmae)
+        .update(this.note)
+        .digest('hex')
     
-    // Hash the password with the salt
-    this.password = await bcrypt.hash(this.password, salt);
+    
     next();
   } catch (error) {
     next(error);
   }
 });
+*/
 
 module.exports = mongoose.model('Password', PasswordSchema);
