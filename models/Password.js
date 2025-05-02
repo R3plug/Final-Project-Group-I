c44b3924ef50c5d4d85c685765c8bb2dc3dc4458
R3plug <phillipwd95@gmail.com>
2025-05-01 17:24:01 -0700

    Started Password model, created password.ejs

---
+++ models/Password.js
@@ -0,0 +1,30 @@
+const mongoose =  require('mongoose');
+const bcrypt = require('bcrypt')
+
+const PasswordSchema = new mongoose.Schema({
+    user:String,
+    username:String,
+    password:String,
+    notes:String,
+    
+});
+
+
+
+PasswordSchema.pre('save', async function(next) {
+  // Only hash the password if it's modified (or new)
+  if (!this.isModified('password')) return next();
+  
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    
+    // Hash the password with the salt
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model('Password', PasswordSchema);
